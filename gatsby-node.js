2a5d2986d9695f9819f0b84e35f42e85ed0230ea
a <a@example.com>
2018-06-25 23:03:14 +0900

    WIP vagrantでホットリロードをできるように

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,23 @@
  */
 
  // You can delete this file if you're not using it
+
+
+// https://github.com/gatsbyjs/gatsby/issues/864#issuecomment-387763111
+function modifyWebpackConfig(wp) {
+  if (wp.stage == "develop") {
+    let config = wp.config;
+    console.log("OPEN THIS: ", "http://"+process.env.PUBLIC_HOST+':'+wp.program.port);
+    if (process.env.PUBLIC_HOST) {
+      config._config.output.publicPath = config._config.output.publicPath.replace('0.0.0.0', process.env.PUBLIC_HOST);
+      config._config.entry.commons = config._config.entry.commons.map(point => {
+        if (/webpack-hot-middleware/.test(point)) {
+        point += '&dynamicPublicPath=true'
+      }
+      return point;
+    })
+    }
+  }
+}
+
+module.exports = {modifyWebpackConfig}
